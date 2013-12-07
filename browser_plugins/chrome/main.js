ed7a34ed4cc81c2ccaa8158aa044fe54a8b28bc1
Michael Bosworth <mtbosworth@gmail.com>
2013-12-07 09:52:42 -0600

    improved template

--- browser_plugins/chrome/main.js
+++ browser_plugins/chrome/main.js
@@ -6,7 +6,15 @@ $(document).on("ready", function() {
 
 	console.log(bg.response);
   	// Render
-  	var template = _.template('<h1><%= name %></h1><p><%= description %></p>');
+
+  	var template = _.template(''+
+  		'<h1><%= name %></h1><p><%= description %></p>' +
+  		'<ul>' +
+  		'<% _.each(steps, function(step) { %>' +
+  		'    <li><a href="javascript:;" class="step"><%= step.title%></a></li>' +
+  		'<% }); %>' +
+  		'</ul>'
+  	);
 
 	$('.content').html(template(bg.response));
 
