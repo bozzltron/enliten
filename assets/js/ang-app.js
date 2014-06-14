ff5337ad9a302152d7b99aeb5a4a4e6ded427a01
Michael Bosworth <mtbosworth@gmail.com>
2014-06-14 18:45:25 -0500

    better formatting ;

--- assets/js/ang-app.js
+++ assets/js/ang-app.js
@@ -5,6 +5,7 @@ require('../bower_components/angular-route/angular-route');
 require('../bower_components/angular-resource/angular-resource');
 require('../bower_components/angular-cookies/angular-cookies');
 require('../bower_components/angular-flash/dist/angular-flash');
+require('../bower_components/angular-animate/angular-animate');
 
 require("./services/path");
 require("./services/status");
@@ -22,7 +23,8 @@ angular.module('enlighten', [
   'enlighten.controllers.nav',
   'enlighten.controllers.editor',
   'angular-flash.service', 
-  'angular-flash.flash-alert-directive'
+  'angular-flash.flash-alert-directive',
+  'ngAnimate'
 	]).config(['$routeProvider', function($routeProvider) {
 
     // Specify routes to load our partials upon the given URLs
