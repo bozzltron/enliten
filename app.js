8fe0b713004c68a05ccac86b981433c13ff44944
Michael Bosworth <michael.bosworth@bunchball.com>
2015-07-26 21:34:21 -0500

    handle exceptions

--- app.js
+++ app.js
@@ -57,3 +57,10 @@ process.chdir(__dirname);
   // Start server
   sails.lift(rc('sails'));
 })();
+
+// catch the uncaught errors that weren't wrapped in a domain or try catch statement
+// do not use this in modules, but only in applications, as otherwise we could have multiple of these bound
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    console.log(err);
+});
